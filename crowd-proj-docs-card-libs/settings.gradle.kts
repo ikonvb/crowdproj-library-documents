67664cece97fe8daa4e92bd6670048edc2412786
ikonvb <korustlt@gmail.com>
2024-12-06 14:15:53 +0300

    added new module crowd-proj-docs-card-lib-logging-common

---
+++ crowd-proj-docs-card-libs/settings.gradle.kts
@@ -0,0 +1,27 @@
+rootProject.name = "crowd-proj-docs-card-libs"
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
+    }
+}
+
+pluginManagement {
+    includeBuild("../crowd-proj-plugin")
+    plugins {
+        id("build-jvm") apply false
+        id("build-kmp") apply false
+    }
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
+
+include(":crowd-proj-docs-card-lib-logging-common")
